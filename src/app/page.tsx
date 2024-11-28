1f426c277685dd12b0b28d1705756979145fef62
Andre Mikheev <andre.mikheev@foodstuffs.co.nz>
2024-11-28 14:10:03 +1300

    install external design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
+import { Heading, tokens } from "@consumer-retail-online-apps/design-system-newworld";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Heading>Heading Component from Design System</Heading>
+
+      <p
+        style={{
+          backgroundColor: tokens.colours.bg.brand.strong,
+          color: tokens.colours.text.inverse,
+          padding: 10,
+        }}
+      >
+        NW Brand Color
+      </p>
+
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
