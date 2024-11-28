1e6fd18221dbc7ab811bc7108af81c408e1a05a4
Andre Mikheev <andre.mikheev@foodstuffs.co.nz>
2024-11-28 14:22:29 +1300

    Revert "install external design system"
    This reverts commit 1f426c277685dd12b0b28d1705756979145fef62.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "@consumer-retail-online-apps/design-system-newworld/lib/style.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
